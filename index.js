8116948556d2b25537a610de85ac9c039c8d0815
Christian Lebrio <ChristianLebrio@gmail.com>
2022-12-25 02:34:04 -0500

    commit tree 288850b6effcd7ef8b95c5f115fefbe9735dddbe

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.getElementById("victory");
+
+newHeader.textContent = "Vrilly is the champion."
